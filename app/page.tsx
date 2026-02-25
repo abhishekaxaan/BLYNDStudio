d52dd5dbd47dd7d248a5d27b50ec01ddc799efa7
abhishekaxaan <abhishekaxaan@gmail.com>
2026-02-26 01:27:30 +0530

    Final cleanup of imports and function names in main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,20 @@
 
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
-import { HeroSection } from "@/components/sections//HeroSection";
-import { CapabilitiesTeaser } from "@/components/sections//CapabilitiesTeaser";
-import { PhilosophySection } from "@/components/sections//PhilosophySection";
-import { ResultsSection } from "@/components/sections//ResultsSection";
-import { ShowcaseSection } from "@/components/sections//ShowcaseSection";
-import { ProcessSection } from "@/components/sections//ProcessSection";
-import { FAQSection } from "@/components/sections//FAQSection";
-import { TechMatrix } from "@/components/sections//TechMatrix";
-import { LaunchButton } from "@/components/sections//LaunchButton";
-import { VisionTicker } from "@/components/sections//VisionTicker";
-import { VisionTickerAlt1 } from "@/components/sections//VisionTickerAlt1";
-import { VisionTickerAlt2 } from "@/components/sections//VisionTickerAlt2";
+import { HeroSection } from "@/components/sections/HeroSection";
+import { CapabilitiesTeaser } from "@/components/sections/CapabilitiesTeaser";
+import { PhilosophySection } from "@/components/sections/PhilosophySection";
+import { ResultsSection } from "@/components/sections/ResultsSection";
+import { ShowcaseSection } from "@/components/sections/ShowcaseSection";
+import { ProcessSection } from "@/components/sections/ProcessSection";
+import { FAQSection } from "@/components/sections/FAQSection";
+import { TechMatrix } from "@/components/sections/TechMatrix";
+import { LaunchButton } from "@/components/sections/LaunchButton";
+import { VisionTicker } from "@/components/sections/VisionTicker";
+import { VisionTickerAlt1 } from "@/components/sections/VisionTickerAlt1";
+import { VisionTickerAlt2 } from "@/components/sections/VisionTickerAlt2";
 
-export default function HomeDemo() {
+export default function Home() {
     return (
         <div className="bg-[#ebf0f5]">
             <Navbar />
